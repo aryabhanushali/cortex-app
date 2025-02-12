732dfbad2c6f6261416b47c2b04b53838dbadac2
Ruolin Wang <violynne10@gmail.com>
2025-02-12 13:02:49 -0500

    updating constants

--- assets/js/constants.jsx
+++ assets/js/constants.jsx
@@ -1,11 +1,11 @@
 export const MODEL_OPTIONS = [
     { value: 'clip_rn50', label: 'Clip-ResNet50' },
-    { value: 'kosmos2', label: 'Kosmos2' },
+    { value: 'dinov2', label: 'dinov2' },
   ];
   
   export const DATASET_OPTIONS = [
     { value: 'murty185', label: 'Murty185' },
-    { value: 'nsd', label: 'NSD' },
+    { value: 'nsd_1000', label: 'NSD' },
   ];
   
   export const REGION_OPTIONS = [
