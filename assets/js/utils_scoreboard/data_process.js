f8b8c2eb9cea90ecd704aa5d7dc420df30fcec07
Yuxuan Li <lyxsunny0801@gmail.com>
2025-03-14 16:37:25 -0400

    update scoreboard

--- assets/js/utils_scoreboard/data_process.js
+++ assets/js/utils_scoreboard/data_process.js
@@ -33,15 +33,45 @@ function loadData(callback) {
         
             return flattened;
         }
+
+        function averageByDataset(data) {
+            let grouped = d3.rollup(
+                data, 
+                v => d3.mean(v, d => d.pearsonr),  // 计算 pearsonr 均值
+                d => d.model,  
+                d => d.dataset, 
+                d => d.roi
+            );
+        
+            console.log("Grouped Data:", grouped); // 调试分组后的数据
+        
+            let flattened = Array.from(grouped, ([model, datasetMap]) => 
+                Array.from(datasetMap, ([dataset, roiMap]) => 
+                    Array.from(roiMap, ([roi, pearsonr]) => ({
+                        model,
+                        dataset,
+                        roi,  // 确保 roi 信息被保留
+                        pearsonr
+                    }))
+                )
+            ).flat(2);
+        
+            console.log("Flattened Data:", flattened); // 调试最终展开的数据
+        
+            return flattened;
+        }
         
 
         const overallData = data.filter(d => d.roi === "Overall");
+        // const filteredData = data.filter(d => d.roi !== "Overall");
         const ppaData = averageByModelROI(data.filter(d => d.roi === "PPA"));
         const ffaData = averageByModelROI(data.filter(d => d.roi === "FFA"));
         const ebaData = averageByModelROI(data.filter(d => d.roi === "EBA"));
+        const allData = averageByModelROI(data.filter(d => d.roi !== "Overall"));
+        
 
        
-        callback(overallData, ppaData, ffaData, ebaData);
+        callback(overallData, ppaData, ffaData, ebaData, allData);
 
     }).catch(error => console.error("❌ Data loading failed:", error));
 }
--- assets/js/utils_scoreboard/draw_chart.js
+++ assets/js/utils_scoreboard/draw_chart.js
@@ -162,6 +162,102 @@ function drawChartOverall(selector, dataset, title) {
       .text(title);
 }
 
+
+function drawChartRoi(selector, dataset, title) {
+
+   const baseFontSize = Math.min(width, height) * 0.02;  
+    
+   const svg = d3.select(selector)
+                 .append("svg")
+                 .attr("width", width)
+                 .attr("height", height);
+
+   const models = Array.from(new Set(dataset.map(d => d.model)));
+   //const datasets = ["NSD", "dataset1", "dataset2", "dataset3"];
+   const rois = Array.from(new Set(dataset.map(d => d.roi)));
+
+
+
+   const xScale = d3.scaleBand()
+                    .domain(rois)
+                    .range([margin.left, width - margin.right])
+                    .padding(0.05);
+
+
+   const yScale = d3.scaleBand()
+                    .domain(models)
+                    .range([margin.top, height - margin.bottom])
+                    .padding(0.05);
+
+  
+   const colorScale = d3.scaleLinear()
+                        .domain([-0.3, 1.0])
+                        .range(["#deebf1", "#3182bd"]);  // 浅蓝到深蓝
+
+
+   const xAxis = svg.append("g")
+      .attr("transform", `translate(0, ${height - margin.bottom})`)
+      .call(d3.axisBottom(xScale));
+
+ 
+   xAxis.selectAll("text")
+      .attr("class", "axis-label clickable") 
+      .style("cursor", "pointer")  // 🆕 鼠标指针变成手型
+      .style("font-size", `${baseFontSize * 1.5}px`)
+      .on("click", function(event, datasetName) {
+         if (selectedRegion === "overall") {
+            window.location.href = `cross-region-performance-page.html?dataset=${datasetName}`;
+         } else {
+            window.location.href = `roi-performance-page.html?dataset=${datasetName}&roi=${selectedRegion}`;
+         }
+      });
+
+   
+   const yAxis = svg.append("g")
+      .attr("transform", `translate(${margin.left}, 0)`)
+      .call(d3.axisLeft(yScale))
+      
+   yAxis.selectAll("text")
+      .attr("class", "axis-label clickable")
+      .style("cursor", "pointer")
+      .style("font-size", `${baseFontSize * 1.5}px`);
+      
+
+
+   const cells = svg.selectAll(".heatmap-cell")
+                    .data(dataset)
+                    .enter()
+                    .append("g")
+                    .attr("class", "heatmap-cell")
+                    .attr("transform", d => `translate(${xScale(d.roi)}, ${yScale(d.model)})`);
+
+   cells.append("rect")
+        .attr("width", xScale.bandwidth())
+        .attr("height", yScale.bandwidth())
+        .attr("fill", d => colorScale(d.pearsonr));
+
+
+   cells.append("text")
+        .attr("x", xScale.bandwidth() / 2)
+        .attr("y", yScale.bandwidth() / 2)
+        .attr("dy", ".35em")
+        .attr("text-anchor", "middle")
+        .attr("fill", "white")  // 白色文字
+        .style("font-size", `${baseFontSize * 1.5}px`)
+
+        .text(d => d.pearsonr.toFixed(2));
+
+
+   svg.append("text")
+      .attr("x", width / 2)
+      .attr("y", 20)
+      .attr("class", "model-label")
+      .attr("text-anchor", "middle")
+      .style("font-size", `${baseFontSize * 1.5}px`)
+      .text(title);
+}
+
+
 // function drawChartOverall(selector, dataset, title) {
 //    const baseFontSize = Math.min(width, height) * 0.02;  
 
