2c4f2ea7aaf53d0091668c668301cbc61a91b85b
Yuxuan Li <lyxsunny0801@gmail.com>
2026-01-07 12:05:14 -0500

    filterbar order: traning, region, dataset

--- assets/js/selectFiltersBar.jsx
+++ assets/js/selectFiltersBar.jsx
@@ -4,21 +4,22 @@ import { Tag } from 'antd';
 const SelectedFiltersBar = ({ training, region, dataset, clearSingle }) => {
   const selected = [];
 
-  // ✅ 加入 training 和 dataset
+  // order: training region dataset
   if (training) selected.push({ key: 'training', label: training, color: '#b8c4de' });
 
+  if (Array.isArray(region) && region.length > 0) {
+    region.forEach((r) => {
+      selected.push({ key: `region-${r}`, label: r.toUpperCase(), color: '#e5cf98' });
+    });
+  }
 
   if (Array.isArray(dataset) && dataset.length > 0) {
     dataset.forEach((r) => {
       selected.push({ key: `dataset-${r}`, label: r.toUpperCase(), color: '#9bbfc4ff' });
     });
   }
-  // ✅ region 是数组，展开
-  if (Array.isArray(region) && region.length > 0) {
-    region.forEach((r) => {
-      selected.push({ key: `region-${r}`, label: r.toUpperCase(), color: '#e5cf98' });
-    });
-  }
+ 
+
 
   return (
     <div
@@ -27,7 +28,7 @@ const SelectedFiltersBar = ({ training, region, dataset, clearSingle }) => {
         flexWrap: 'wrap',
         gap: 0,
         padding: '0',
-        minHeight: 30, // ✅ 保证顶部栏占位不跳动
+        minHeight: 30, 
         alignItems: 'center',
       }}
     >
@@ -37,7 +38,7 @@ const SelectedFiltersBar = ({ training, region, dataset, clearSingle }) => {
           color={item.color}
           closable
           onClose={() => {
-            // ✅ 如果是 region，就单独移除
+            // region/ dataset support single remove
             if (item.key.startsWith('region-')) {
               const r = item.key.replace('region-', '');
               clearSingle('region', r);
