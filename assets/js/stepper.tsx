e29cfe7dd5aaa208a671ab5f6433a8ef0e13a6ed
Ruolin Wang <49186492+v1o6ynne@users.noreply.github.com>
2026-03-04 13:21:33 -0500

    handling step2 prediction

--- assets/js/stepper.tsx
+++ assets/js/stepper.tsx
@@ -138,11 +138,11 @@ const Stepper: React.FC = () => {
   // }, [files]);
 
   // Trigger prediction when settings change or files are uploaded
-  // useEffect(() => {
-  //   setPredictionResult(null); // Clear previous results
-  //   setPredictstep(1); // Reset button
-  //   handlePrediction();
-  // }, [model, dataset, region, voxelOption, voxelNumber, paper, participantName]);
+  useEffect(() => {
+    setPredictionResult(null); // Clear previous results
+    setPredictstep(1); // Reset button
+    handlePrediction();
+  }, [model, dataset, region, voxelOption, voxelNumber, paper, participantName]);
 
   useEffect(() => {
     // Only auto-trigger prediction if we're on Step 3 (index 2)
