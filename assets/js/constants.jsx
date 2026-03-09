33b677f6f3d726ee46dd0ddbf453881c4e6530ed
Yuxuan Li <lyxsunny0801@gmail.com>
2026-03-09 14:50:40 -0400

    the lab region option updated for vss (backend only connect for ppa ffa eba)

--- assets/js/constants.jsx
+++ assets/js/constants.jsx
@@ -851,8 +851,8 @@ export const PAPER_OPTIONS = [
 ];
 
 //Regions included in each dataset
-export const MURTY185_INCLUDED_REGIONS = ['ffa', 'eba', 'ppa'];
-export const NSD_1000_INCLUDED_REGIONS = ['ffa', 'eba', 'ppa'];
+export const MURTY185_INCLUDED_REGIONS = ['ffa', 'eba', 'ppa', 'fba', 'ofa', 'opa', 'rsc'];
+export const NSD_1000_INCLUDED_REGIONS = ['ffa', 'eba', 'ppa', 'fba', 'ofa', 'opa', 'rsc', 'vwfa'];
 
 //Modelcard info
 export const MODELCARD_INFO_LOOKUP = {
